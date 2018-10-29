ce2354c32cb4b4b805ac32b6e1c5ae831f9d308b
nlopezm <nlopezmarini@gmail.com>
2018-10-29 17:56:33 -0300

    Listado de clientes

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { using } from 'rxjs';
 
 @Component({
   selector: 'app-clientes',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./clientes.component.scss']
 })
 export class ClientesComponent {
+
+  search = '';
+  clientesTotales: any = [
+    { id: 1, nombre: 'Nahuel', apellido: 'López', documento: 4520 },
+    { id: 2, nombre: 'Nacho', apellido: 'Fili' },
+    { id: 3, nombre: 'Ke', apellido: 'Cat' },
+  ];
+  clientes: any;
+  loading = false;
+
+  constructor() {
+    this.clientes = this.clientesTotales;
+  }
+
+  deleteCliente(i: number) {
+
+  }
+
+  searchCliente() {
+    this.clientes = this.clientesTotales.filter((cliente) => {
+      const nombre = (cliente.nombre + ' ' + cliente.apellido).toLowerCase();
+      return (nombre.indexOf(this.search.toLowerCase()) > -1)
+        || (cliente.documento && cliente.documento.toString().indexOf(this.search.toLowerCase()) > -1);
+    });
+  }
 
 }
