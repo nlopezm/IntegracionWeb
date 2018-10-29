2d033d30447dea524408ccf3975f0524a9620481
nachofilippi <nachofilippi@live.com.ar>
2018-10-29 11:20:43 -0300

    'hola'

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
+
+export interface Animal {
+  name: string;
+  sound: string;
+}
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
   styleUrls: ['./clientes.component.scss']
 })
-export class ClientesComponent implements OnInit {
+export class ClientesComponent{
+  productos = new FormControl();
+ 
+  productosList: string[] = ['39048329473242374', '984127412471','7381267438129367'];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  startDate = new Date(1940, 0, 1);
 }
