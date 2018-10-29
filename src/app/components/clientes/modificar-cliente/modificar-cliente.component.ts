a96b92a740cf63b8af3a7f50b38e11d3af2607c7
nachofilippi <nachofilippi@live.com.ar>
2018-10-29 18:37:44 -0300

    modificaciones listas

--- src/app/components/clientes/modificar-cliente/modificar-cliente.component.ts
+++ src/app/components/clientes/modificar-cliente/modificar-cliente.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-modificar-cliente',
   templateUrl: './modificar-cliente.component.html',
   styleUrls: ['./modificar-cliente.component.scss']
 })
-export class ModificarClienteComponent implements OnInit {
+export class ModificarClienteComponent  {
 
-  constructor() { }
+  startDate = new Date();
 
-  ngOnInit() {
+  formGroup: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formGroup = this.fb.group({
+      nombre: ['', [Validators.required]],
+      apellido: ['', [Validators.required]],
+      tipo: ['', [Validators.required]],
+      numero: ['', [Validators.required]],
+      nacimiento: ['', [Validators.required]],
+    });
   }
 
 }
