22f07b50ae22d4afa18efd3f9cda00eacf8da1e0
nlopezm <nlopezmarini@gmail.com>
2018-10-29 18:36:17 -0300

    Alta consumo

--- src/app/components/consumos/consumos.component.ts
+++ src/app/components/consumos/consumos.component.ts
@@ -7,31 +7,7 @@ import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/fo
   styleUrls: ['./consumos.component.scss']
 })
 export class ConsumosComponent implements OnInit {
-  clientes: any = [
-    { id: 1, nombre: 'Nahuel', apellido: 'López', documento: 4520 },
-    { id: 2, nombre: 'Nacho', apellido: 'Fili' },
-    { id: 3, nombre: 'Ke', apellido: 'Cat' },
-  ];
-  clientesFiltered: any = [];
-  formGroup: FormGroup;
+  ngOnInit(): void {
 
-  constructor(private fb: FormBuilder) {
-    this.formGroup = this.fb.group({
-      monto: ['', [Validators.required]],
-      cliente: ['', [Validators.required]],
-    });
-    this.clientesFiltered = this.clientes;
   }
-
-  ngOnInit() {
-  }
-
-  filter(search) {
-    this.clientesFiltered = this.clientes.filter((cliente) => {
-      const nombre = (cliente.nombre + ' ' + cliente.apellido).toLowerCase();
-      return (nombre.indexOf(search.toLowerCase()) > -1)
-        || (cliente.documento && cliente.documento.toString().indexOf(search.toLowerCase()) > -1);
-    });
-  }
-
 }
