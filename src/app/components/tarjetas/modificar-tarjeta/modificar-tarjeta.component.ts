101206a2d263b1d18a4efeaaf85c4f2fb6eaa8dc
nachofilippi <nachofilippi@live.com.ar>
2018-10-29 18:04:02 -0300

    primer commit de modificar tarjeta

---
+++ src/app/components/tarjetas/modificar-tarjeta/modificar-tarjeta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modificar-tarjeta',
+  templateUrl: './modificar-tarjeta.component.html',
+  styleUrls: ['./modificar-tarjeta.component.scss']
+})
+export class ModificarTarjetaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
