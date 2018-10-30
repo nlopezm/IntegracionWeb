5d26a1963fdcd9997b5cf496f401a5069cc4df9a
nachofilippi <nachofilippi@live.com.ar>
2018-10-30 12:27:20 -0300

    kecat branch

--- src/app/components/liquidaciones/liquidaciones.component.ts
+++ src/app/components/liquidaciones/liquidaciones.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-liquidaciones',
   templateUrl: './liquidaciones.component.html',
   styleUrls: ['./liquidaciones.component.scss']
 })
-export class LiquidacionesComponent implements OnInit {
+export class LiquidacionesComponent{
+search = '';
+  liquidacionesTotales: any = [
+    { id: 1, fechaCierre: '20/03/2019', fechaVencimiento: '18/04/2019' },
+    { id: 2, fechaCierre: '10/03/2019', fechaVencimiento: '25/03/2019' },
+    { id: 3, fechaCierre: '01/02/2019', fechaVencimiento: '30/03/2019' },
+  ];
+  liquidaciones: any;
+  tarjetasCred: any = [
+    { id: 4444000033335555 },
+    { id: 4332134233335555 },
+    { id: 4484127864262838 },
+  ];
+  tarjetas: any;
+  loading = false;
+  formGroup: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    this.liquidaciones = this.liquidacionesTotales;
+    this.tarjetas = this.tarjetasCred;
+    this.formGroup = this.fb.group({
+      numeroDocumento: ['', [Validators.required]],
+      anioCierre: ['', [Validators.required, Validators.min(1980), Validators.max(2019)]],
+      anioVencimiento: ['', [Validators.required, Validators.min(1980), Validators.max(2019)]],
+      tarjeta: ['', [Validators.required]],
+    });
   }
+
+  
 
 }
