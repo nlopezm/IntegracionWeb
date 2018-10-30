75789518a5dfde9995ce005df55c701acad61fbd
nlopezm <nlopezmarini@gmail.com>
2018-10-30 20:12:27 -0300

    Crear consumo

--- src/app/components/consumos/crear-consumo/crear-consumo.component.ts
+++ src/app/components/consumos/crear-consumo/crear-consumo.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { ClienteService } from '../../../services/rest/cliente.service';
+import { EstablecimientoService } from '../../../services/rest/establecimiento.service';
+import { ConsumoService } from '../../../services/rest/consumo.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-crear-consumo',
@@ -7,31 +11,71 @@ import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/fo
   styleUrls: ['./crear-consumo.component.scss']
 })
 export class CrearConsumoComponent implements OnInit {
-  clientes: any = [
-    { id: 1, nombre: 'Nahuel', apellido: 'López', documento: 4520 },
-    { id: 2, nombre: 'Nacho', apellido: 'Fili' },
-    { id: 3, nombre: 'Ke', apellido: 'Cat' },
-  ];
+  clientes: any = [];
   clientesFiltered: any = [];
+  tarjetas: any = [];
+  tarjetasFiltered: any = [];
+  establecimientos: any = [];
+  establecimientosFiltered: any = [];
   formGroup: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private clienteService: ClienteService,
+    private establecimientoService: EstablecimientoService,
+    private consumoService: ConsumoService, private router: Router) {
     this.formGroup = this.fb.group({
       monto: ['', [Validators.required]],
-      cliente: ['', [Validators.required]],
+      clienteId: ['', [Validators.required]],
+      tarjetaId: ['', [Validators.required]],
+      fecha: ['', [Validators.required]],
+      idEstablecimiento: ['', [Validators.required]],
+      codigoSeguridad: ['', [Validators.required]],
     });
     this.clientesFiltered = this.clientes;
   }
 
   ngOnInit() {
+    this.clienteService.getClientes().subscribe(
+      (data) => { this.clientes = data; this.clientesFiltered = data; }, () => { }
+    );
+    this.establecimientoService.getEstablecimientos().subscribe(
+      (data) => { this.establecimientos = data; this.establecimientosFiltered = data; }, () => { }
+    );
   }
 
-  filter(search) {
+  filterClientes(search) {
     this.clientesFiltered = this.clientes.filter((cliente) => {
       const nombre = (cliente.nombre + ' ' + cliente.apellido).toLowerCase();
       return (nombre.indexOf(search.toLowerCase()) > -1)
         || (cliente.documento && cliente.documento.toString().indexOf(search.toLowerCase()) > -1);
     });
+  }
+
+  filterTarjetas(search) {
+    this.tarjetasFiltered = this.tarjetas.filter((tarjeta) => tarjeta.nroTarjeta === search);
+  }
+
+  onClienteSelected(event) {
+    const clienteId = event.option.value;
+    const cliente = this.clientes.filter((cliente) => cliente.id === clienteId)[0];
+    this.tarjetas = cliente.tarjetas || [];
+    this.tarjetasFiltered = this.tarjetas;
+  }
+
+  filterEstablecimientos(search) {
+    this.establecimientosFiltered = this.establecimientos.filter((establecimiento) => {
+      return (establecimiento.razonSocial.toLowerCase().indexOf(search.toLowerCase()) > -1);
+    });
+  }
+
+  create() {
+    if (!this.formGroup.valid)
+      return;
+    this.consumoService.postConsumo(this.formGroup.value).subscribe(
+      () => {
+        this.router.navigateByUrl('/consumos');
+        alert('Consumo creado!');
+      }, () => alert('Hubo un error. Volvé a intentarlo.')
+    );
   }
 
 }
