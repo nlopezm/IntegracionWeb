76072a1902b9647aaf4919b6096d733e5fe274de
nlopezm <nlopezmarini@gmail.com>
2018-10-30 18:48:26 -0300

    .

--- src/app/components/liquidaciones/liquidaciones.component.ts
+++ src/app/components/liquidaciones/liquidaciones.component.ts
@@ -6,13 +6,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './liquidaciones.component.html',
   styleUrls: ['./liquidaciones.component.scss']
 })
-export class LiquidacionesComponent{
-search = '';
-// liquidaciones
+export class LiquidacionesComponent {
+  search = '';
+  // liquidaciones
   liquidacionesTotales: any = [
-    { id: 1, mesCierre: new Date('20/03/2019').getMonth, fechaCierre: '20/03/2019', fechaVencimiento: '18/04/2019'},
-    { id: 2, mesCierre: new Date('10/03/2019').getMonth, fechaCierre: '10/03/2019', fechaVencimiento: '25/03/2019'},
-    { id: 3, mesCierre: new Date('01/02/2019').getMonth, fechaCierre: '01/02/2019', fechaVencimiento: '30/03/2019'},
+    { id: 1, mesCierre: new Date('20/03/2019').getMonth, fechaCierre: '20/03/2019', fechaVencimiento: '18/04/2019' },
+    { id: 2, mesCierre: new Date('10/03/2019').getMonth, fechaCierre: '10/03/2019', fechaVencimiento: '25/03/2019' },
+    { id: 3, mesCierre: new Date('01/02/2019').getMonth, fechaCierre: '01/02/2019', fechaVencimiento: '30/03/2019' },
   ];
   liquidaciones: any;
   // tarjetas
@@ -24,12 +24,12 @@ search = '';
   tarjetas: any;
   // consumos
   consumosTotales: any = [
-    { fecha: '22/03/2017', establecimiento:'Correas de distribucion Filippi', descripcion:'Lazos', montototal: 300, cols: 2, rows:2 },
-    { fecha: '23/10/2015', establecimiento:'Supermercado Piti', descripcion:'Botines', montototal: 200, cols: 2, rows:2  },
-    { fecha: '21/02/2017', establecimiento:'Gimnasio GUAU', descripcion:'Pesas', montototal: 30, cols: 2, rows:2  },
-    { fecha: '02/09/2007', establecimiento:'KEKE seguros', descripcion:'Faraones', montototal: 100000000000, cols: 2, rows:2 },
-    { fecha: '10/04/2017', establecimiento:'Japon Tech', descripcion:'Chocolates', montototal: 400, cols: 2, rows:2  },
-    { fecha: '12/08/2017', establecimiento:'Jefe Drones', descripcion:'Los springsboks', montototal: 300, cols: 2, rows:2  },
+    { fecha: '22/03/2017', establecimiento: 'Correas de distribucion Filippi', descripcion: 'Lazos', montototal: 300, cols: 2, rows: 2 },
+    { fecha: '23/10/2015', establecimiento: 'Supermercado Piti', descripcion: 'Botines', montototal: 200, cols: 2, rows: 2 },
+    { fecha: '21/02/2017', establecimiento: 'Gimnasio GUAU', descripcion: 'Pesas', montototal: 30, cols: 2, rows: 2 },
+    { fecha: '02/09/2007', establecimiento: 'KEKE seguros', descripcion: 'Faraones', montototal: 100000000000, cols: 2, rows: 2 },
+    { fecha: '10/04/2017', establecimiento: 'Japon Tech', descripcion: 'Chocolates', montototal: 400, cols: 2, rows: 2 },
+    { fecha: '12/08/2017', establecimiento: 'Jefe Drones', descripcion: 'Los springsboks', montototal: 300, cols: 2, rows: 2 },
   ];
   consumos: any;
   panelOpenState = false;
@@ -61,13 +61,5 @@ search = '';
       tarjeta: ['', [Validators.required]],
     });
   }
-
-  // filtroMesCierre() {
-  //   this.liquidaciones = this.liquidacionesTotales.filter((liquidaciones) => {
-  //     const mesCierre = (this.liquidaciones.mesCierre);
-  //     return (mesCierre.indexOf(this.search.toLowerCase()) > -1)
-  //       || (mesCierre.documento && mesCierre.documento.toString().indexOf(this.search.toLowerCase()) > -1);
-  //   });
-  // }
 
 }
