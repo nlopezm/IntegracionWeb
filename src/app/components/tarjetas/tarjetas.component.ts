acf400a7eb5f1816a0bde35a30207c04849f8f7d
nlopezm <nlopezmarini@gmail.com>
2018-10-30 17:55:13 -0300

    Ver tarjetas rest

--- src/app/components/tarjetas/tarjetas.component.ts
+++ src/app/components/tarjetas/tarjetas.component.ts
@@ -1,29 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { ConfirmDialogComponent } from 'src/app/components/dialogs/confirm-dialog/confirm-dialog.component';
+import { TarjetaService } from '../../services/rest/tarjeta.service';
 
 @Component({
   selector: 'app-tarjetas',
   templateUrl: './tarjetas.component.html',
   styleUrls: ['./tarjetas.component.scss']
 })
-export class TarjetasComponent {
+export class TarjetasComponent implements OnInit {
 
   search = '';
-  tarjetas: any = [
-    { id: 1, numero: '1562130306516', apellido: 'López', documento: 4520 },
-    { id: 2, numero: '498412106532', apellido: 'Fili' },
-    { id: 3, numero: '789415612021', apellido: 'Cat' },
-  ];
+  tarjetas: any;
   tarjetasFiltered: any;
   loading = false;
 
-  constructor(public dialog: MatDialog) {
+  constructor(public dialog: MatDialog, private rest: TarjetaService) {
     this.tarjetasFiltered = this.tarjetas;
   }
 
-  deleteTarjeta(i: number) {
-    this.dialog.open(ConfirmDialogComponent, {
+  ngOnInit() {
+    this.loading = true;
+    this.rest.getTarjetas().subscribe(
+      (data) => {
+        this.tarjetas = data;
+        this.tarjetasFiltered = data;
+        this.loading = false;
+      }, () => this.loading = false
+    );
+
+  }
+
+  deleteTarjeta(id: number) {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
       data: {
         titulo: 'Eliminar tarjeta',
         descripcion: 'Estás seguro que querés eliminar esta tarjeta?',
@@ -31,6 +40,17 @@ export class TarjetasComponent {
           { text: 'Cancelar', function: function () { console.log('Cancelado'); } },
           { text: 'Aceptar', function: function () { console.log('Borrado'); } }
         ]
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.rest.deleteTarjeta(id).subscribe(
+          () => {
+            this.tarjetas = this.tarjetas.filter((tarjeta) => tarjeta.id !== id);
+            this.tarjetasFiltered = this.tarjetas.filter((tarjeta) => tarjeta.id !== id);
+            alert('Tarjeta borrada');
+          }, (e) => console.log(e)
+        );
       }
     });
 
