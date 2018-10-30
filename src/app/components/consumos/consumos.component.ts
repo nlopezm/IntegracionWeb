b5195f5f5f19c46c43790036cf5cc74787838d33
nlopezm <nlopezmarini@gmail.com>
2018-10-30 09:00:28 -0300

    Listado consumos

--- src/app/components/consumos/consumos.component.ts
+++ src/app/components/consumos/consumos.component.ts
@@ -7,7 +7,24 @@ import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/fo
   styleUrls: ['./consumos.component.scss']
 })
 export class ConsumosComponent implements OnInit {
+
+  search = '';
+  consumos: any = [
+    { id: 1, nombre: 'Consumo 1', apellido: 'López', documento: 4520 },
+    { id: 2, nombre: 'Consumo 2', apellido: 'Fili' },
+    { id: 3, nombre: 'Consumo 3', apellido: 'Cat' },
+  ];
+  consumosFiltered: any;
+  loading = false;
+
+  constructor() {
+    this.consumosFiltered = this.consumos;
+  }
+
   ngOnInit(): void {
+  }
+
+  deleteConsumos(i: number) {
 
   }
 }
