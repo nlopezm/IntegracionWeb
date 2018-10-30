a755068b4118a2229609aaa2263dab2689ea6a44
nlopezm <nlopezmarini@gmail.com>
2018-10-30 10:18:14 -0300

    Vista tarjetas

--- src/app/components/tarjetas/tarjetas.component.ts
+++ src/app/components/tarjetas/tarjetas.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { ConfirmDialogComponent } from 'src/app/components/dialogs/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-tarjetas',
   templateUrl: './tarjetas.component.html',
   styleUrls: ['./tarjetas.component.scss']
 })
-export class TarjetasComponent implements OnInit {
+export class TarjetasComponent {
 
-  constructor() { }
+  search = '';
+  tarjetas: any = [
+    { id: 1, numero: '1562130306516', apellido: 'López', documento: 4520 },
+    { id: 2, numero: '498412106532', apellido: 'Fili' },
+    { id: 3, numero: '789415612021', apellido: 'Cat' },
+  ];
+  tarjetasFiltered: any;
+  loading = false;
 
-  ngOnInit() {
+  constructor(public dialog: MatDialog) {
+    this.tarjetasFiltered = this.tarjetas;
   }
 
+  deleteTarjeta(i: number) {
+    this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        titulo: 'Eliminar tarjeta',
+        descripcion: 'Estás seguro que querés eliminar esta tarjeta?',
+        buttons: [
+          { text: 'Cancelar', function: function () { console.log('Cancelado'); } },
+          { text: 'Aceptar', function: function () { console.log('Borrado'); } }
+        ]
+      }
+    });
+
+  }
+
+  searchTarjeta() {
+    this.tarjetasFiltered = this.tarjetas.filter((tarjeta) => {
+      return (tarjeta.numero.toLowerCase().indexOf(this.search.toLowerCase()) > -1);
+    });
+  }
 }
