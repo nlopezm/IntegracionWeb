b6118a02dbc1aec8d9e0fe76a9f217f71bc45654
nlopezm <nlopezmarini@gmail.com>
2018-10-31 09:31:13 -0300

    ver tarjeta

--- src/app/components/tarjetas/modificar-tarjeta/modificar-tarjeta.component.ts
+++ src/app/components/tarjetas/modificar-tarjeta/modificar-tarjeta.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TarjetaService } from '../../../services/rest/tarjeta.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { RestService } from '../../../services/rest/rest.service';
 
 @Component({
   selector: 'app-modificar-tarjeta',
   templateUrl: './modificar-tarjeta.component.html',
   styleUrls: ['./modificar-tarjeta.component.scss']
 })
-export class ModificarTarjetaComponent {
+export class ModificarTarjetaComponent implements OnInit {
 
-  hola = 'PUTO';
+  tarjeta: any;
+  tarjetaId: number;
+  loading = false;
+  cliente: string;
 
-  constructor() {
+  constructor(private tarjetaService: TarjetaService, private router: Router,
+    private route: ActivatedRoute, private rest: RestService) {
+    this.tarjetaId = this.route.snapshot.params.tarjetaId;
+  }
 
+  ngOnInit() {
+    this.loading = true;
+    this.tarjetaService.getTarjeta(this.tarjetaId).subscribe(
+      (data) => {
+        this.rest.get(data['_links'].cliente.href).subscribe(
+          (data) => { this.tarjeta.cliente = data; this.cliente = data['nombre'] + ' ' + data['apellido']; }, () => { }
+        );
+        this.tarjeta = data;
+        this.loading = false;
+        this.tarjeta['fechaValidaDesde'] = new Date(data['fechaValidaDesde']);
+        this.tarjeta['fechaValidaHasta'] = new Date(data['fechaValidaHasta']);
+      }, () => this.loading = false
+    );
+  }
+
+  goBack() {
+    this.router.navigateByUrl('/tarjetas');
   }
 
 }
