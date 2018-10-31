c78878c8671fbff544552bcc1ac96f0ae7ec0c12
nlopezm <nlopezmarini@gmail.com>
2018-10-31 10:54:22 -0300

    Get consumos

--- src/app/components/consumos/consumos.component.ts
+++ src/app/components/consumos/consumos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { ConsumoService } from '../../services/rest/consumo.service';
 
 @Component({
   selector: 'app-consumos',
@@ -9,19 +10,22 @@ import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/fo
 export class ConsumosComponent implements OnInit {
 
   search = '';
-  consumos: any = [
-    { id: 1, nombre: 'Consumo 1', apellido: 'López', documento: 4520 },
-    { id: 2, nombre: 'Consumo 2', apellido: 'Fili' },
-    { id: 3, nombre: 'Consumo 3', apellido: 'Cat' },
-  ];
+  consumos: any = [];
   consumosFiltered: any;
   loading = false;
 
-  constructor() {
-    this.consumosFiltered = this.consumos;
+  constructor(private consumoService: ConsumoService) {
   }
 
   ngOnInit(): void {
+    this.loading = true;
+    this.consumoService.getConsumos().subscribe(
+      (data) => {
+        this.consumos = data;
+        this.consumoService = data;
+        this.loading = false;
+      }, () => this.loading = false
+    );
   }
 
   deleteConsumos(i: number) {
