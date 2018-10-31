94d9e1e50819690bfc39b37ed8d7f23016ab58f7
nlopezm <nlopezmarini@gmail.com>
2018-10-31 08:59:38 -0300

    Fecha nacimiento cliente

--- src/app/components/clientes/crear-cliente/crear-cliente.component.ts
+++ src/app/components/clientes/crear-cliente/crear-cliente.component.ts
@@ -2,18 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { ClienteService } from '../../../services/rest/cliente.service';
 import { Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-crear-cliente',
   templateUrl: './crear-cliente.component.html',
-  styleUrls: ['./crear-cliente.component.scss']
+  styleUrls: ['./crear-cliente.component.scss'],
+  providers: [DatePipe]
 })
 export class CrearClienteComponent {
   startDate = new Date();
 
   formGroup: FormGroup;
 
-  constructor(private fb: FormBuilder, private rest: ClienteService, private router: Router) {
+  constructor(private fb: FormBuilder, private rest: ClienteService, private router: Router, public datepipe: DatePipe) {
     this.formGroup = this.fb.group({
       nombre: ['', [Validators.required]],
       apellido: ['', [Validators.required]],
@@ -24,9 +26,12 @@ export class CrearClienteComponent {
   }
 
   crear() {
-    if (!this.formGroup.valid)
+    if (!this.formGroup.valid) {
       return;
-    this.rest.postCliente(this.formGroup.value).subscribe(
+    }
+    const cliente = this.formGroup.value;
+    cliente['fechaNacimiento'] = this.datepipe.transform(cliente['fechaNacimiento'], 'yyyy-MM-dd');
+    this.rest.postCliente(cliente).subscribe(
       (res) => {
         this.router.navigateByUrl('/clientes');
         alert('Cliente creado!');
